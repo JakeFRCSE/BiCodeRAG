4fe18253a24bd36c149198e191efcf5a110234a6
JakeFRCSE <eppi001004@gmail.com>
2025-02-17 18:20:04 +0900

    fix: Edit llamaCrossAttention
    Fixed the shape of key_states and value_states for the length of cross_attention_states.
    Deleted position embedding so that all of the cross_attention_states can be attended.
    Deleted kv_cache for decoder's previous key value.
    Need to implement kv_cache for encoder's key value.

--- src/models/llama/modeling_llama.py
+++ src/models/llama/modeling_llama.py
@@ -315,7 +315,7 @@ class LlamaCrossAttention(nn.Module):
         self.num_key_value_groups = config.num_attention_heads // config.num_key_value_heads
         self.scaling = self.head_dim**-0.5
         self.attention_dropout = config.attention_dropout
-        self.is_causal = True
+        self.is_causal = False
 
         self.q_proj = nn.Linear(
             config.hidden_size, config.num_attention_heads * self.head_dim, bias=config.attention_bias
@@ -334,7 +334,6 @@ class LlamaCrossAttention(nn.Module):
         self,
         hidden_states: torch.Tensor,
         cross_attention_states: torch.Tensor,
-        position_embeddings: Tuple[torch.Tensor, torch.Tensor],
         attention_mask: Optional[torch.Tensor],
         past_key_value: Optional[Cache] = None,
         cache_position: Optional[torch.LongTensor] = None,
@@ -342,13 +341,12 @@ class LlamaCrossAttention(nn.Module):
     ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
         input_shape = hidden_states.shape[:-1]
         hidden_shape = (*input_shape, -1, self.head_dim)
+        cross_input_shape = cross_attention_states.shape[:-1]
+        cross_hidden_shape = (*cross_input_shape, -1, self.head_dim)
 
         query_states = self.q_proj(hidden_states).view(hidden_shape).transpose(1, 2)
-        key_states = self.k_proj(cross_attention_states).view(hidden_shape).transpose(1, 2)
-        value_states = self.v_proj(cross_attention_states).view(hidden_shape).transpose(1, 2)
-
-        cos, sin = position_embeddings
-        query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
+        key_states = self.k_proj(cross_attention_states).view(cross_hidden_shape).transpose(1, 2)
+        value_states = self.v_proj(cross_attention_states).view(cross_hidden_shape).transpose(1, 2)
 
         attention_interface: Callable = eager_attention_forward
         if self.config._attn_implementation != "eager":
