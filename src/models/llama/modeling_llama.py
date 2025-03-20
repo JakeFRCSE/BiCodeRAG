718013bbb325be4ddfcf2f66689cdf44d385d11e
jake <eppi001004@gmail.com>
2025-03-20 14:53:19 +0900

    fix:Typo.

--- src/models/llama/modeling_llama.py
+++ src/models/llama/modeling_llama.py
@@ -1365,7 +1365,6 @@ class LlamaForCausalLM(LlamaPreTrainedModel, GenerationMixin):
 
         hidden_states = outputs[0]
 
-        #TODO: align tensor of logits and labels
         # Only compute necessary logits, and do not upcast them to float if we are not computing the loss
         slice_indices = slice(-logits_to_keep, None) if isinstance(logits_to_keep, int) else logits_to_keep
         logits = self.lm_head(hidden_states[:, slice_indices, :])
